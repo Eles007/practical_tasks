459a75a15ebc543b3bb63ca09404cb6e979e5887
Eles007 <lyos00@mail.ru>
2026-02-04 15:09:39 +0400

    Добавил базу данных

--- mydb.sql
+++ mydb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Фев 01 2026 г., 15:35
+-- Время создания: Фев 04 2026 г., 12:09
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -32,8 +32,7 @@ CREATE TABLE `feedback` (
   `name` varchar(255) NOT NULL,
   `topic` enum('question','problem','proposal') NOT NULL,
   `contact` enum('email','phone') NOT NULL DEFAULT 'email',
-  `agree_news` tinyint(1) DEFAULT 0,
-  `agree_data` tinyint(1) NOT NULL DEFAULT 0,
+  `agree` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`agree`)),
   `message` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -42,10 +41,11 @@ CREATE TABLE `feedback` (
 -- Дамп данных таблицы `feedback`
 --
 
-INSERT INTO `feedback` (`id`, `name`, `topic`, `contact`, `agree_news`, `agree_data`, `message`, `created_at`) VALUES
-(29, 'Иван2', 'problem', 'phone', 0, 1, '123', '2026-02-01 14:24:21'),
-(30, 'Иван3', 'proposal', 'phone', 1, 1, '1234545', '2026-02-01 14:24:34'),
-(31, 'Иван2', 'problem', 'phone', 0, 1, '123124', '2026-02-01 14:34:06');
+INSERT INTO `feedback` (`id`, `name`, `topic`, `contact`, `agree`, `message`, `created_at`) VALUES
+(4, 'user', 'problem', 'phone', '[\"news\",\"data\"]', 'it’s not working', '2026-02-04 10:27:38'),
+(5, 'user', 'problem', 'phone', '[\"data\"]', 'it’s not working', '2026-02-04 10:29:12'),
+(6, 'user', 'problem', 'phone', '[\"data\"]', 'it’s not working', '2026-02-04 10:29:37'),
+(7, 'Иван2', 'question', 'phone', '[\"news\",\"data\"]', ';;;dsfsdf\'\'asdasdqwe\'\'\'\'\'\'\'\'', '2026-02-04 10:35:12');
 
 -- --------------------------------------------------------
 
@@ -65,7 +65,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `created_at`) VALUES
-(1, 'user', '$2y$10$YeyimnmFRr4uAWu1DQYtWem/T9FDwuXqGqfvI3JhizEf9nxjxqPX6', '2026-02-01 11:34:04');
+(1, 'user', '$2y$10$YeyimnmFRr4uAWu1DQYtWem/T9FDwuXqGqfvI3JhizEf9nxjxqPX6', '2026-02-04 08:40:32');
 
 --
 -- Индексы сохранённых таблиц
@@ -92,7 +92,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
