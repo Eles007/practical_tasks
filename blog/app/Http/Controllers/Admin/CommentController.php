ac0819ca3584f5b46cfe01b18ba72ccf6ef33880
Eles007 <lyos00@mail.ru>
2026-02-10 18:43:42 +0400

    Add Admin CommentController

---
+++ blog/app/Http/Controllers/Admin/CommentController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $comments = Comment::with('post')
+            ->latest()
+            ->paginate(15);
+    }
+
+    public function approve(Comment $comment)
+    {
+        $comment->update(['status' => 'approved']);
+
+        return back()->with('success', 'Комментарий одобрен');
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return back()->with('success', 'Комментарий удалён');
+    }
+}
