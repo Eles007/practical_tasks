bab4502891d38f5b53db10c890835d26eca91809
Eles007 <lyos00@mail.ru>
2026-02-11 23:20:31 +0400

    Add CRUD

--- php/blog/app/Http/Controllers/PostController.php
+++ php/blog/app/Http/Controllers/PostController.php
@@ -3,22 +3,99 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class PostController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index(): View
     {
-        $posts = Post::query()
-            ->where('is_published', true)
-            ->orderByDesc('published_at')
-            ->paginate(6);
-        return view('posts.index', compact('posts'));
+        $posts = Post::latest()->paginate(5);
+
+        return view('admin.posts.index', compact('posts'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $data = $request->validate([
+            'title' => 'required|min:3',
+            'slug' => 'nullable',
+            'excerpt' => 'required|min:10',
+            'body' => 'required|min:10',
+            'is_published' => 'nullable',
+            'published_at' => 'nullable',
+            'user_id' => 'nullable',
+        ]);
+
+        Post::create($data);
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success', 'Пост успешно создан');
+    }
+
+    /**
+     * Display the specified resource.
+     */
     public function show(Post $post): View
     {
-        return view('posts.show', compact('post'));
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post): View
+    {
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post): RedirectResponse
+    {
+        $data = $request->validate([
+            'title' => 'required|min:3',
+            'slug' => 'nullable',
+            'excerpt' => 'required|min:10',
+            'body' => 'required|min:10',
+            'is_published' => 'nullable',
+            'published_at' => 'nullable',
+            'user_id' => 'nullable',
+        ]);
+
+        $post->update($data);
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success', 'Пост успешно обновлен');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post): RedirectResponse
+    {
+        $post->delete();
+
+        return redirect()
+            ->route('posts.index')
+            ->with('success', 'Пост успешно удалён');
     }
 }
